84da5300258d9ef5ee009985ab19c161fa249560
vstarhosting <vstarhosting@gmail.com>
2025-03-11 23:55:06 +0530

    next config edited

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export', // Keeps static export
+  images: {
+    unoptimized: true, // Disable image optimization
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
